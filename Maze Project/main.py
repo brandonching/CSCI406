5f3bc1bd8697c3de4b4c38c94b0bc71d933bbbff
Brandon Ching <bching9@gmail.com>
2024-02-01 12:33:50 -0700

    change to explicit graph definition

--- Maze Project/main.py
+++ Maze Project/main.py
@@ -1,6 +1,14 @@
-# import graph library
+############################################
+# Name: Maze Project
+#
+# The S.S. Fearless was on a routine spaceflight when it was attacked by space pirates. In a fierce battle, Captain Rocket and Lieutenant Lucky defeated the pirates, but their ship's Master Computer was damaged in the fight. Rocket is trapped in room A and Lucky trapped in room B. They cannot leave their rooms to enter the adjacent corridors because the life support system (normally controlled by the Master Computer) is now inoperative.
+# Lucky has a brilliant idea: use the manual override in each room to temporarily activate the corridor's life support sys-tem. Each room can activate the life support in any corridor of the same color. However, an operator must remain in the room while the corridor is being used. Each corridor is constructed for travel in only one direction, so whoever walks through a corridor must travel in the direction of the arrow. If either Rocket or Lucky can make it to the Master Computer, he can repair it; otherwise, they both will remain lost in space. How can one of them get to the Master Computer-at the point marked "Goal" on the maze?
+# Here's an example to show how they can move through the spaceship. Since Captain Rocket starts in a purple room, he could operate the controls to enable Lucky to travel along the purple corridor from room B to room G. Lucky would now be in a green room and he could operate the controls to let Rocket travel through the green corridor from room A to room J. Lucky could next move to K; Rocket could move to 0; and Lucky could move to F. With Lucky now in a green room, Rocket could move down the green corridor to N; then he could make another move along the green corridor to T. (Note that you don't always have to alternate between Rocket and Lucky.) Captain Rocket is now getting close to the Master Computer and things are looking hopeful. But, alas, if you continue with this example you'll find that both space travelers will soon be trapped in endless loops. Whenever that happens, you'll just have to start over.
+#
+# Author:  Brandon Ching
+############################################
+
 import networkx as nx
-# import matplotlib.pyplot as plt
 import sys
 
 # if argument provided, get the input file from the argument
@@ -29,181 +37,63 @@ room_colors.append("goal")
 captain_start = int(lines[2].split()[0])
 lieutenant_start = int(lines[2].split()[1])
 
-
-# create a map, where the key is the color string and the value is a list of rooms with that color
-room_map = {}
-for i in range(rooms-1):
-    if room_colors[i] in room_map:
-        room_map[room_colors[i]].append(i)
-    else:
-        room_map[room_colors[i]] = [i]
-
-# create a map, where the key is the room number and the value is currently blank
-room_map2 = {}
-for i in range(rooms-1):
-    room_map2[i] = []
-
-
 # Create a graph
-game_board = nx.DiGraph()
+game_states = nx.DiGraph()
 
-# add a node for each room (skip 0)
-for i in range(rooms, 0, -1):
-    game_board.add_node(i, color=room_colors[i-1], node_color='lightblue')
+# create a node for every possible game state (each game state is a tuple of 2 rooms corresponding to the captain and lieutenant's positions)
+for i in range(rooms-1, 0, -1):
+    for j in range(rooms-1, 0, -1):
+        game_states.add_node((i, j))
 
-# change the node_color for the starting rooms and goal
-game_board.nodes[captain_start]['node_color'] = 'red'
-game_board.nodes[lieutenant_start]['node_color'] = 'green'
-game_board.nodes[rooms]['node_color'] = 'yellow'
+# create a single node for the goal state
+game_states.add_node((rooms, rooms))
 
+# read in the rest of the input file, where each line represents a corridor between two rooms and the color of the corridor
+for i in range(corridors):
+    corridor = lines[i+3].split()
+    origin = int(corridor[0])
+    destination = int(corridor[1])
+    color = corridor[2]
 
-# finish reading the input file, where the line format is "room1 room2 color"
-# make edges directed
-for i in range(3, len(lines)):
-    room1 = int(lines[i].split()[0])
-    room2 = int(lines[i].split()[1])
-    color = lines[i].split()[2]
+    # for each game state, if the lieutenant is in the origin room and the captain is in a room with the same color as the corridor, add an edge to the game state where the lieutenant is in the destination room and the captain is in the same room as before
+    for j in range(rooms, 0, -1):
+        if (j, origin) in game_states.nodes and color == room_colors[j-1]:
+            # if destination is the goal room, add an edge to the goal state
+            if destination == rooms:
+                game_states.add_edge(
+                    (j, origin), (rooms, rooms), name="L" + str(rooms))
+            else:
+                game_states.add_edge(
+                    (j, origin), (j, destination), name="L" + str(destination))
 
-    # add an edge between the two rooms
-    game_board.add_edge(room1, room2, color=color)
+    # for each game state, if the captain is in the origin room and the lieutenant is in a room with the same color as the corridor, add an edge to the game state where the captain is in the destination room and the lieutenant is in the same room as before
+    for j in range(rooms, 0, -1):
+        if (origin, j) in game_states.nodes and color == room_colors[j-1]:
+            # if destination is the goal room, add an edge to the goal state
+            if destination == rooms:
+                game_states.add_edge(
+                    (origin, j), (rooms, rooms), name="R" + str(rooms))
+            else:
+                game_states.add_edge(
+                    (origin, j), (destination, j), name="R" + str(destination))
 
+# check if there are any paths from the starting game state to the goal state
+if not nx.has_path(game_states, (captain_start, lieutenant_start), (rooms, rooms)):
+    print("NO PATH")
+    sys.exit()
 
-# add labels for the edges
-edge_labels = nx.get_edge_attributes(game_board, 'color')
-room_labels = nx.get_node_attributes(game_board, 'color')
+# find all shortest paths from the starting game state to the goal state
+paths = nx.all_shortest_paths(
+    game_states, (captain_start, lieutenant_start), (rooms, rooms))
 
+# convert the paths to strings
+output = []
+for path in paths:
+    path_output = ""
+    for i in range(len(path)-1):
+        path_output += game_states[path[i]][path[i+1]]['name']
+    output.append(path_output)
 
-# draw the graph with labels
-# pos = nx.spring_layout(game_board)
-# nx.draw(game_board, pos, with_labels=True, verticalalignment='top', node_color=[game_board.nodes[i]['node_color'] for i in game_board.nodes],
-#        node_size=1500, font_weight='bold')
-# nx.draw_networkx_edge_labels(game_board, pos, edge_labels=edge_labels)
-# add room labels under the number
-# nx.draw_networkx_labels(game_board, pos, labels=room_labels, font_size=8,
-#                         font_color='black', font_weight='bold', verticalalignment='bottom')
-
-
-# Create a Game State class
-
-class GameState:
-    def __init__(self, board_graph, captain_location, lieutenant_location, move_name):
-        self.board = board_graph
-        self.captain = captain_location
-        self.lieutenant = lieutenant_location
-        self.move_name = move_name
-
-    # function to check if the game is over
-    def is_goal(self):
-        if self.board.nodes[self.captain]['color'] == 'goal' or self.board.nodes[self.lieutenant]['color'] == 'goal':
-            return True
-        else:
-            return False
-
-    # function to get the possible moves from current game state
-    def get_possible_moves(self):
-        # Get a list of the rooms that the captain can move to. Captain can move through any edge with the same color as lieutenant's room
-        captain_moves = list(self.board.successors(self.captain))
-        captain_valid_moves = []
-        for item in captain_moves:
-            if self.board.get_edge_data(self.captain, item)['color'] == self.board.nodes[self.lieutenant]['color']:
-                captain_valid_moves.append(item)
-
-        # Get a list of the rooms that the lieutenant can move to. Lieutenant can move through any edge with the same color as captain's room
-        lieutenant_moves = list(self.board.successors(self.lieutenant))
-        lieutenant_valid_moves = []
-        for item in lieutenant_moves:
-            if self.board.get_edge_data(self.lieutenant, item)['color'] == self.board.nodes[self.captain]['color']:
-                lieutenant_valid_moves.append(item)
-
-        # Create a list of game states that can be reached from the current game state
-        game_states = []
-        for item in lieutenant_valid_moves:
-            game_states.append(
-                GameState(self.board, self.captain, item, "L" + str(item)))
-        for item in captain_valid_moves:
-            game_states.append(
-                GameState(self.board, item, self.lieutenant, "R" + str(item)))
-
-        return game_states
-
-    # function to print the game state
-    def print_state(self):
-        print("Captain: ", self.captain, " Lieutenant: ",
-              self.lieutenant)
-
-    def move_name(self):
-        return self.move_name
-
-    def __eq__(self, other):
-        return self.captain == other.captain and self.lieutenant == other.lieutenant
-
-
-# Create a new game state
-game = GameState(game_board, captain_start, lieutenant_start, "Start")
-
-# plot
-# plt.show()
-# Conduct a BFS search to find the shortest path to the goal do not revisit nodes
-
-
-def bfs_search(game_state):
-    queue = []
-    visited = []
-    queue.append([game_state])
-    while queue:
-        path = queue.pop(0)
-        node = path[-1]
-        if node.is_goal():
-            return path
-        if node not in visited:
-            for next_node in node.get_possible_moves():
-                new_path = list(path)
-                new_path.append(next_node)
-                queue.append(new_path)
-            visited.append(node)
-    return None
-
-# Conduct a BFS search to find all paths to the goal do not revisit nodes and store the paths in a list
-
-
-def bfs_search_all(game_state):
-    queue = []
-    visited = []
-    queue.append([game_state])
-    paths = []
-    while queue:
-        path = queue.pop(0)
-        node = path[-1]
-        if node.is_goal():
-            paths.append(path)
-        if node not in visited:
-            for next_node in node.get_possible_moves():
-                new_path = list(path)
-                new_path.append(next_node)
-                queue.append(new_path)
-            visited.append(node)
-    return paths
-
-
-# create a map of the shortest paths, where the key is the length of the path and the value is is list of paths
-shortest_paths = {}
-if bfs_search(game) is None:
-    shortest_paths[0] = ["NO PATH"]
-for i in bfs_search_all(game):
-    path = ""
-    for j in range(1, len(i)):
-        path += i[j].move_name
-    if len(i) in shortest_paths:
-        shortest_paths[len(i)].append(path)
-    else:
-        shortest_paths[len(i)] = [path]
-
-# Get the list of the shortest paths from the map
-shortest_paths = shortest_paths[min(shortest_paths.keys())]
-
-# lexographically sort the paths and return the first one
-shortest_paths.sort()
-shortest_paths = shortest_paths[0]
-
-# print the paths
-print(shortest_paths)
+# sort all paths lexicographically and print the first one
+output.sort()
+print(output[0])
