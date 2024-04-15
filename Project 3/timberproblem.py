20921f7dd9e38f78b103ab88eb88dd045bc21ce5
Brandon Ching <bching9@gmail.com>
2024-04-15 12:29:21 -0600

    complete part 3, pass all test

--- Project 3/timberproblem.py
+++ Project 3/timberproblem.py
@@ -45,6 +45,67 @@ def timber_bottom_up(log_sizes):
     return table[0][n - 1]
 
 
+def timber_bottom_up_traverse(log_sizes):
+    '''
+    This function solves the timber problem using a bottom-up approach
+    :param log_sizes: A list of log sizes
+    :return: The maximum value that can be obtained from cutting the logs
+    '''
+    n = len(log_sizes)
+    # Prefix sum of the log sizes
+    sums = [0] * n
+    for i in range(n):
+        sums[i] = log_sizes[i] + (sums[i - 1] if i > 0 else 0)
+
+    # Create a table to store the results and fill in the base case (i == j)
+    table = [[0 for _ in range(n)] for _ in range(n)]
+    parent_table = [[(0, 0) for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        table[i][i] = log_sizes[i]
+        parent_table[i][i] = (i, i)
+
+    # Fill in the table using the bottom-up approach (traverse diagonally)
+    for diag in range(1, n):
+        for i in range(n - diag):
+            j = i + diag
+            table[i][j] = sums[j] - (sums[i - 1] if i > 0 else 0) - \
+                min(table[i + 1][j], table[i][j - 1])
+            if (table[i + 1][j] == table[i][j - 1]):
+                parent_table[i][j] = (i+1, j)
+            elif (table[i + 1][j] < table[i][j - 1]):
+                parent_table[i][j] = (i + 1, j)
+            else:
+                parent_table[i][j] = (i, j-1)
+
+    print(table[0][n - 1])
+
+    # Traverse the table to get the logs that were cut
+    # start in the top right corner
+
+    # Print out the traversal, start in the top right corner
+    next_i = 0
+    next_j = n - 1
+    next_log = parent_table[next_i][next_j]
+    for log in range(n):
+        # if the next log points to the same log, we are done
+        if next_log == (next_i, next_j):
+            print(next_i + 1, end=" ")
+
+        # if the next log points to the log below
+        elif next_log[0] == next_i + 1:
+            print(next_i + 1, end=" ")
+            next_i = next_log[0]
+            next_log = parent_table[next_i][next_j]
+
+        # if the next log points to the log to the left
+        elif next_log[1] == next_j - 1:
+            print(next_j + 1, end=" ")
+            next_j = next_log[1]
+            next_log = parent_table[next_i][next_j]
+
+    print()
+
+
 def command_line_input():
     '''
     This function reads the input file from the command line and runs 
@@ -61,7 +122,7 @@ def command_line_input():
 
     # Get the sizes of the logs from the second line and run the algorithm
     log_sizes = list(map(int, lines[1].split()))
-    print(timber_bottom_up(log_sizes))
+    timber_bottom_up_traverse(log_sizes)
 
 
 def synthetic_test(max_size=20, test_count=100):
@@ -140,7 +201,7 @@ def validate_methods():
 
 if __name__ == "__main__":
     # Uncomment the line below to run the command line input
-    # command_line_input()  # This one is used for grading
+    command_line_input()  # This one is used for grading
     # synthetic_test()
-    synthetic_test_bottom_up()
+    # synthetic_test_bottom_up()
     # validate_methods()
